352f1d87c23616eb3979097055f20746a22ff447
KarolWes <karol.wesolowski01@gmail.com>
2022-07-08 14:50:08 +0200

    Bąbelki generowane w trakcie ruchu do przodu/tyłu. Nie wiem dlaczego nie działa animacja, mimo że na wykresie się pokazuje. Wyłączyłem moduł ruchu

---
+++ Assets/Scripts/SubmarineAnimCntr.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubmarineAnimCntr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Animator _animator;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.W))
+        {
+            if (_animator.GetCurrentAnimatorStateInfo (1).normalizedTime > 1 && !_animator.IsInTransition (1))
+            {
+                _animator.SetTrigger ("spin");
+            }
+        }
+        if (Input.GetKey (KeyCode.S))
+        {
+            if (_animator.GetCurrentAnimatorStateInfo (1).normalizedTime > 1 && !_animator.IsInTransition (1))
+            {
+                _animator.SetTrigger ("spin_back");
+            }
+        }
+        if (Input.GetKeyDown (KeyCode.A))
+        {
+            _animator.SetTrigger ("left");
+        }
+        if (Input.GetKeyUp (KeyCode.A))
+        {
+            _animator.SetTrigger ("left_back");
+        }
+        if (Input.GetKeyDown (KeyCode.D))
+        {
+            _animator.SetTrigger ("right");
+        }
+        if (Input.GetKeyUp (KeyCode.D))
+        {
+            _animator.SetTrigger ("right_back");
+        }
+        
+    }
+}
