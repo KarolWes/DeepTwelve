d2b7ca4f53c0ab67c4c611ae0663804ee679bdbc
szbanek <stefan.zbaszyniak.gim4@gmail.com>
2022-07-09 17:08:13 +0200

    naprawienie torped i uporządkowanie prefabów

--- Assets/Scripts/TorpedoMovement.cs
+++ Assets/Scripts/TorpedoMovement.cs
@@ -4,16 +4,19 @@ using System.Threading.Tasks;
 using UnityEngine;
 
 public class TorpedoMovement : MonoBehaviour {
-    private TorpedoManager _tm;
+    //private TorpedoManager _tm;
     [SerializeField] private float maxDist = 5f;
     [SerializeField] private float speed = 5f;
+    private Vector3 goal;
     void Start() {
-        Vector3 pos = transform.position;
-        _tm = FindObjectOfType<TorpedoManager> ();
+        //_tm = FindObjectOfType<TorpedoManager> ();
         transform.localScale = new Vector3 (0.4f, 0.6f, 0.4f);
-        Vector3 goal = pos + new Vector3 (0, 0, -maxDist);
-        transform.position = Vector3.MoveTowards (pos, goal, speed*Time.deltaTime); // nie działa
-        Destroy (this);
+        goal = transform.position + new Vector3 (0, 0, maxDist);
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards (transform.position, goal, speed*Time.deltaTime); 
     }
 
 }
--- Assets/Scripts/bubble_controler.cs
+++ Assets/Scripts/bubble_controler.cs
@@ -4,18 +4,11 @@ using UnityEditor;
 using UnityEngine;
 
 public class bubble_controler : MonoBehaviour {
-    [SerializeField] private GameObject submarine;
-    private Vector3 _pos;
     public ParticleSystem ps;
-    void Start() {
-        _pos = submarine.transform.position +  new Vector3(0f, 0f, 3f);
-    }
 
     // Update is called once per frame
     void Update()
     {
-        _pos = submarine.transform.position +  new Vector3(0f, 0f, 3f);
-        transform.position = _pos;
         if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.S))
         {
             if (!ps.isEmitting)
