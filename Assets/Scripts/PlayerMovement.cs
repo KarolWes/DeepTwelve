c7cddced654483f27e3f2cc28a28b0ebaed88111
KarolWes <karol.wesolowski01@gmail.com>
2022-08-06 13:15:45 +0200

    ruch gracza Nie jest idealnie, oś obrotu powinna być przesunięta bardziej do tyłu, ale nie wiem jak to zrobić + bąbelki się nie przesuwają, więc to napraw!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    private int move = 0;
+    private int deg = 40;
     void Start()
     {
         
@@ -24,20 +26,28 @@ public class PlayerMovement : MonoBehaviour
         }
         if(Input.GetKey(KeyCode.W))
         {
-            goal += transform.forward;
-        }
-        if(Input.GetKey(KeyCode.S))
-        {
             goal -= transform.forward;
+            move = 1;
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            goal += transform.forward;
+            move = -1;
+        }
+        else
+        {
+            move = 0;
         }
         if(Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(0,100*Time.deltaTime,0);
+            transform.Rotate(0,-deg*Time.deltaTime*move,0);
         }
         if(Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(0,-100*Time.deltaTime,0);
+            transform.Rotate(0,deg*Time.deltaTime*move,0);
         }
+
+        //transform.Rotate(transform.LookAt (goal));
         transform.position = Vector3.MoveTowards(transform.position, goal, 4f * Time.deltaTime);
     }
 }
